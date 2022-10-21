782e2dee3e5b01f5ce950a3d3c22befd8f3e63f7
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-21 14:10:35 +0700

    Create Catagory in API

---
+++ DucHanh_Assignment_MidTerm/API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace API.Models
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
