7740b7a9837bc06fadf3489cc00a8dbb5ee7ddf4
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-21 14:14:31 +0700

    Create model Product  in API

---
+++ DucHanh_Assignment_MidTerm/API/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace API.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Uses { get; set; }
+        public string Describe { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public double ImportPrice { get; set; }
+        public double Price { get; set; }
+        public string CreatedDate { get; set; }
+        public string UpdatedDate { get; set; }
+        public Category category { get; set; }
+    }
+}
