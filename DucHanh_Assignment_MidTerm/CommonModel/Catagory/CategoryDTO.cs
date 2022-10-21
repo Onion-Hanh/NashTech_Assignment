e887f4aa1ca3e6eb8225d8d0d62e8d99e4011a14
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-21 14:26:38 +0700

    Create Model CategoryDTO and ProductDTO

---
+++ DucHanh_Assignment_MidTerm/CommonModel/Catagory/CategoryDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonModel.Catagory
+{
+    public class Category
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
---
+++ DucHanh_Assignment_MidTerm/CommonModel/Product/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonModel.Catagory;
+
+namespace CommonModel.Product
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Content { get; set; }
+        public string Uses { get; set; }
+        public string Describe { get; set; }
+        public string Image { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string CategoryID { get; set; }
+
+    }
+}
