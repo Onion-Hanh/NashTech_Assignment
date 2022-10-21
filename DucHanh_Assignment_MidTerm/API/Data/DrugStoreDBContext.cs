fb6079888a61fb02b2e9f618331adec37e79b481
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-21 17:01:02 +0700

    update DrugStoreDBContext to use Fluent API

--- DucHanh_Assignment_MidTerm/API/Data/DrugStoreDBContext.cs
+++ DucHanh_Assignment_MidTerm/API/Data/DrugStoreDBContext.cs
@@ -1,11 +1,10 @@
-﻿using CommonModel;
+﻿using API.Models;
 using Microsoft.EntityFrameworkCore;
-
 namespace API.Data
 {
     public class DrugStoreDBContext : DbContext
     {
-        public DbSet<Catagory> cataogies { get; set; }
+        public DbSet<Category> categories { get; set; }
         public DbSet<Product> products { get; set; }
         public DrugStoreDBContext(DbContextOptions<DrugStoreDBContext> options)
             : base(options)
@@ -19,6 +18,23 @@ namespace API.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            //add primary key for category
+            modelBuilder.Entity<Category>()
+                .HasKey(c => c.Id);
+            //add primary key for product
+            modelBuilder.Entity<Product>()
+                .HasKey(c => c.Id);
+            //add foreign key for product
+            modelBuilder.Entity<Product>()
+                .HasOne<Category>(c => c.Category)
+                .WithMany(g=>g.products)
+                .HasForeignKey(s => s.CategoryId);
+            //add cascade delete for category
+            modelBuilder.Entity<Category>()
+                .HasMany<Product>(c => c.products)
+                .WithOne(g => g.Category)
+                .HasForeignKey(c => c.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
         }
     }
 }
