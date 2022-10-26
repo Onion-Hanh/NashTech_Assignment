c5ee6cee8f4b65d4ef1197045819a33db84709e0
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-26 15:37:23 +0700

    update for mapping

--- DucHanh_Assignment_MidTerm/API/Program.cs
+++ DucHanh_Assignment_MidTerm/API/Program.cs
@@ -1,4 +1,7 @@
 using API.Data;
+using API.Interfaces;
+using API.Mapping;
+using API.Repositories;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +17,11 @@ builder.Services.AddDbContext<DrugStoreDBContext>(options =>
 {
     options.UseSqlServer(connectionString);
 });
+//config automapper
+builder.Services.AddAutoMapper(typeof(Category_Mapping).Assembly);
+builder.Services.AddScoped<ICategory_Repository, Category_Repository>();
+builder.Services.AddAutoMapper(typeof(Product_Mapping).Assembly);
+builder.Services.AddScoped<IProduct_Repository, Product_Repository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
