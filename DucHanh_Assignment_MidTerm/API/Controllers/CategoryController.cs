dacb96af5d6b5b482c773f6f7769b90680358a09
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-26 15:31:34 +0700

    Update CategeroryController and create  new ProductController

---
+++ DucHanh_Assignment_MidTerm/API/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using API.Interfaces;
+using CommonModel.Category;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        public readonly ICategory_Repository _category_Repository;
+        public CategoryController(ICategory_Repository category_Repository)
+        {
+            _category_Repository = category_Repository;
+        }
+        [HttpGet]
+        public async Task<List<CategoryDTO>> getAllCategories()
+        {
+            return await _category_Repository.GetCategories();
+        }
+    }
+}
---
+++ DucHanh_Assignment_MidTerm/API/Controllers/ProductController.cs
@@ -0,0 +1,54 @@
+﻿using API.Interfaces;
+using CommonModel.Product;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.AccessControl;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        public readonly IProduct_Repository _product_Repository;
+        public ProductController(IProduct_Repository product_Repository)
+        {
+            _product_Repository = product_Repository;
+        }
+        [HttpGet]
+        public async Task<List<ProductDTO>> getAllProducts()
+        {
+            return await _product_Repository.GetProducts();
+        }
+        [HttpGet("{categoryId}")]
+        public async Task<IActionResult> getAllProductsByCategoryId(string categoryId)
+        {
+            var products = await _product_Repository.GetProdctsByCategoryId(categoryId);
+            if (products == null)
+            {
+                return BadRequest();
+            }
+            return Ok(products);
+        }
+        [HttpGet("[action]/{productId}")]
+        public async Task<IActionResult> getProductById(string productId)
+        {
+            var product = await _product_Repository.GetProductDetail(productId);
+            if (product == null)
+            {
+                return BadRequest();
+            }
+            return Ok(product);
+        }
+        [HttpGet("[action]/{productName}")]
+        public async Task<IActionResult> getProductsByName(string productName)
+        {
+            var products = await _product_Repository.GetProductsByName(productName);
+            if (products == null)
+            {
+                return BadRequest();
+            }
+            return Ok(products);
+        }
+    }
+}
