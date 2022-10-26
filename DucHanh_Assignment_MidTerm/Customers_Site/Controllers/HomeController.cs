3b0b29990ee5331770f2ac9479b99bb0ea1177c4
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-26 15:39:46 +0700

    create some new method like getListCategories, getListProducts, getListProductsByCategoryId, getProductsByName(Search)

--- DucHanh_Assignment_MidTerm/Customers_Site/Controllers/HomeController.cs
+++ DucHanh_Assignment_MidTerm/Customers_Site/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using Customers_Site.Models;
+﻿using CommonModel.Category;
+using CommonModel.Product;
+using Customers_Site.Interfaces;
+using Customers_Site.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,23 +9,37 @@ namespace Customers_Site.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICategory_Service _category_Service;
+        private readonly IProduct_Service _product_Service;
+        List<CategoryDTO> listCategory = new List<CategoryDTO>();
+        List<ProductDTO> listProduct = new List<ProductDTO>();
+        public HomeController(ICategory_Service category_Service, IProduct_Service product_Service)
         {
-            _logger = logger;
+            _category_Service = category_Service;
+            _product_Service = product_Service;
         }
-
-        public IActionResult Index()
+        public async Task<IActionResult> Index(string categoryId)
         {
+            ViewBag.categoryId = categoryId;
             return View();
         }
-
-        public IActionResult Privacy()
+        public async Task<IActionResult> ProductDetail(string productId)
         {
+            ViewBag.productId = productId;
             return View();
         }
-
+        public async Task<IActionResult> SearchProduct(IFormCollection form)
+        {
+            listProduct.Clear();
+            string productName = form["search"];
+            listProduct = await _product_Service.getProductsByName(productName);
+            ViewBag.productName = productName;
+            if (listProduct.Count == 0)
+            {
+                ViewBag.search = "Không có sản phẩm theo mục tìm kiếm";
+            }
+            return View("Index", listProduct);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
