96a6b12aad125abce073db4db2ba1e4d08a945c2
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-26 15:41:36 +0700

    Update Service folder

---
+++ DucHanh_Assignment_MidTerm/Customers_Site/Services/Category_Service.cs
@@ -0,0 +1,29 @@
+﻿using CommonModel.Category;
+using Customers_Site.Interfaces;
+using Newtonsoft.Json;
+using System.Data.Common;
+
+namespace Customers_Site.Services
+{
+    public class Category_Service : ICategory_Service
+    {
+        IHttpClientFactory _factory;
+        List<CategoryDTO> categories;
+        public Category_Service(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+        public async Task<List<CategoryDTO>> getListCategories()
+        {
+            categories = new List<CategoryDTO>();
+            var httpClient = _factory.CreateClient();
+            var responseMessage = await httpClient.GetAsync("Category");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var data = await responseMessage.Content.ReadAsStringAsync();
+                categories = JsonConvert.DeserializeObject<List<CategoryDTO>>(data);
+            }
+            return categories;
+        }
+    }
+}
---
+++ DucHanh_Assignment_MidTerm/Customers_Site/Services/Product_Service.cs
@@ -0,0 +1,65 @@
+﻿using API.Models;
+using CommonModel.Product;
+using Customers_Site.Interfaces;
+using Newtonsoft.Json;
+
+namespace Customers_Site.Services
+{
+    public class Product_Service : IProduct_Service
+    {
+        IHttpClientFactory _factory;
+        List<ProductDTO> products;
+        public Product_Service(IHttpClientFactory factory)
+        {
+            _factory = factory;
+        }
+        public async Task<List<ProductDTO>> getListProducts()
+        {
+            products = new List<ProductDTO>();
+            var httpClient = _factory.CreateClient();
+            var reponseMessage = await httpClient.GetAsync("Product");
+            if (reponseMessage.IsSuccessStatusCode)
+            {
+                var data = await reponseMessage.Content.ReadAsStringAsync();
+                products = JsonConvert.DeserializeObject < List<ProductDTO>>(data);
+            }
+            return products;
+        }
+        public async Task<List<ProductDTO>> getProductsByCategoryId(string categoryId)
+        {
+            products = new List<ProductDTO>();
+            var httpClient = _factory.CreateClient();
+            var reponseMessage = await httpClient.GetAsync("Product/" + categoryId);
+            if (reponseMessage.IsSuccessStatusCode)
+            {
+                var data = await reponseMessage.Content.ReadAsStringAsync();
+                products = JsonConvert.DeserializeObject<List<ProductDTO>>(data);
+            }
+            return products;
+        }
+        public async Task<ProductDTO> getProductById(string productId)
+        {
+            ProductDTO product = new ProductDTO();
+            var httpClient = _factory.CreateClient();
+            var reponseMessage = await httpClient.GetAsync("Product/getProductById/" + productId);
+            if (reponseMessage.IsSuccessStatusCode)
+            {
+                var data = await reponseMessage.Content.ReadAsStringAsync();
+                product = JsonConvert.DeserializeObject<ProductDTO>(data);
+            }
+            return product;
+        }
+        public async Task<List<ProductDTO>> getProductsByName(string productName)
+        {
+            products = new List<ProductDTO>();
+            var httpClient = _factory.CreateClient();
+            var reponseMessage = await httpClient.GetAsync("Product/getProductsByName/" + productName);
+            if (reponseMessage.IsSuccessStatusCode)
+            {
+                var data = await reponseMessage.Content.ReadAsStringAsync();
+                products = JsonConvert.DeserializeObject<List<ProductDTO>>(data);
+            }
+            return products;
+        }
+    }
+}
