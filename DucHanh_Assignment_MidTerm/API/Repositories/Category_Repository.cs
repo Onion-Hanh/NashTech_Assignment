8c0edfe35f9c5afc89b137d3b954df1b3b7b1bb7
Nguyen Duc Hanh <onionsummer42@gmail.com>
2022-10-26 15:35:51 +0700

    update Category_Repository and create new Product_Repository

---
+++ DucHanh_Assignment_MidTerm/API/Repositories/Category_Repository.cs
@@ -0,0 +1,26 @@
+﻿using API.Data;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CommonModel.Category;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class Category_Repository:ICategory_Repository
+    {
+        public readonly DrugStoreDBContext _drugStoreDBContext;
+        public readonly IMapper _mapper;
+        public Category_Repository(DrugStoreDBContext drugStoreDBContext, IMapper mapper)
+        {
+            _drugStoreDBContext = drugStoreDBContext;
+            _mapper = mapper;
+        }
+
+        public async Task<List<CategoryDTO>> GetCategories()
+        {
+            return await _drugStoreDBContext.categories.ProjectTo<CategoryDTO>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+
+    }
+}
---
+++ DucHanh_Assignment_MidTerm/API/Repositories/Product_Repository.cs
@@ -0,0 +1,37 @@
+﻿using API.Data;
+using API.Interfaces;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using CommonModel.Product;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Repositories
+{
+    public class Product_Repository : IProduct_Repository
+    {
+        public readonly DrugStoreDBContext _drugStoreDBContext;
+        public IMapper _mapper;
+        public Product_Repository(DrugStoreDBContext drugStoreDBContext, IMapper mapper)
+        {
+            _drugStoreDBContext = drugStoreDBContext;  
+            _mapper = mapper;
+        }
+
+        public async Task<List<ProductDTO>> GetProducts()
+        {
+            return await _drugStoreDBContext.products.ProjectTo<ProductDTO>(_mapper.ConfigurationProvider).ToListAsync();
+        }
+        public async Task<List<ProductDTO>> GetProdctsByCategoryId(string categoryId)
+        {          
+            return await _drugStoreDBContext.products.ProjectTo<ProductDTO>(_mapper.ConfigurationProvider).Where(c=>c.CategoryID== categoryId).ToListAsync();
+        }
+        public async Task<ProductDTO> GetProductDetail(string productId)
+        {
+            return await _drugStoreDBContext.products.ProjectTo<ProductDTO>(_mapper.ConfigurationProvider).Where(c => c.Id == productId).FirstOrDefaultAsync();
+        }
+        public async Task<List<ProductDTO>> GetProductsByName(string productName)
+        {
+            return await _drugStoreDBContext.products.ProjectTo<ProductDTO>(_mapper.ConfigurationProvider).Where(c => c.Name.Contains(productName)).ToListAsync();
+        }
+    }
+}
